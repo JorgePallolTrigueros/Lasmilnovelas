1d39546066329581cfb219be158ee4ae75ac902b
Jonathan Hernandez <hv12i04001@gmail.com>
2021-08-18 21:30:50 +0200

    agregada servicio de guardado img

---
+++ src/main/java/com/Lasmilnovelas/service/ImageStoreService.java
@@ -0,0 +1,65 @@
+package com.example.image;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Service
+public class ImageStoreService {
+
+
+    private final Path root = Paths.get("imagenes");
+
+    public void init() {
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Error al inicializar la carpeta "+root.toString());
+        }
+    }
+
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            throw new RuntimeException("Error no se pudo guardar el archivo: " + e.getMessage());
+        }
+    }
+
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Error al leer el archivo");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al cargar archivos");
+        }
+    }
+
+}
